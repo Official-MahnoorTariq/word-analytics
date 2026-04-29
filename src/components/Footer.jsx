f12f8ce83b758b283e8225f752d4553a7dfaaf7d
Official-MahnoorTariq <real.mahnoor.tariq@gmail.com>
2026-04-29 19:10:32 +0500

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+        <small>
+            &copy; Copyright by ByteGrad. all rights reserved.
+        </small>
+        <small>
+            last checked limits: 17 days ago
+        </small>
+        </footer>
+  )
+}
